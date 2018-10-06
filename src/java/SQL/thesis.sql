40f6cab66316502b51ab9e009d472c90a1ce486a
Dino Alcala <dino12225@gmail.com>
2018-10-06 16:31:42 +0800

    new sql

--- src/java/SQL/thesis.sql
+++ src/java/SQL/thesis.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `thsis01` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `thsis01`;
 -- MySQL dump 10.13  Distrib 5.7.23, for Win64 (x86_64)
 --
 -- Host: localhost    Database: thsis01
@@ -1050,4 +1052,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-10-06 16:28:33
+-- Dump completed on 2018-10-06 16:31:11
