4d63dab3b871f4725d1a1fc548f0682cf4fa1527
Dino Alcala <dino12225@gmail.com>
2018-11-21 20:23:33 +0800

    fixed notifs for when submitting accomplishment report

--- src/java/controller/createFFreport5.java
+++ src/java/controller/createFFreport5.java
@@ -80,10 +80,20 @@ public class createFFreport5 extends HttpServlet {
             java.text.SimpleDateFormat sdf = new java.text.SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 
             n.setDt(sdf.format(dt));
-            n.setUserID(17);
-            
+            n.setUserID(UserDAO.getUserIDforNotifsPosition("OVPLM - Vice President for Lasallian Mission"));
             UserDAO.AddNotification(n);
             
+            n.setTitle(UserDAO.getProgramName(FFreport.getFfproposalID()));
+            n.setBody("Accomplishment Report has been submitted!");
+            n.setDt(sdf.format(dt));
+            n.setUserID(UserDAO.getUserIDforNotifsPosition("OVPLM - Executive Officer"));
+            UserDAO.AddNotification(n);
+            
+            n.setTitle(UserDAO.getProgramName(FFreport.getFfproposalID()));
+            n.setBody("Accomplishment Report has been submitted!");
+            n.setDt(sdf.format(dt));
+            n.setUserID(UserDAO.getUserIDforNotifsPosition("OVPLM - Sir Jay Position"));
+            UserDAO.AddNotification(n);
             
             request.setAttribute("FFreport", "You have successfully created the Accomplishment Report!");
             ServletContext context = getServletContext();
